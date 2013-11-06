0f1c321b44b20637b4e38c32f336764c1463edc5
chloeclalad <hxie34@gatech.edu>
2013-11-06 01:53:39 -0500

    Auto Panning and Zooming
    got-data.js is in new format with family encoded;
    update your video file;
    panning and zooming are working.

--- data/got-data.js
+++ data/got-data.js
@@ -5,63 +5,208 @@ exports.eventInfo = [
         "data": {
             "locationX": 400,
             "locationY": 400,
-            "title": "Intro Scene",
-            "characters": ["Solder 1", "Soldier 2"]
-        }
-    },
+            "characters": [{ "name": "Solder_1", "family": "lannister"},
+                           { "name": "Solder_2", "family": "lannister"}]
+    
+    }
+},
 	{
 		"event": 1,
 		"type": "moment",
         "data": {
-            "title": "Moment about the mountain"
-        }
-    },
-	{
-		"event": 2,
-		"type": "moment",
-        "data": {
-            "title": "Moment about relation between Renly and Loras"
-        }
-    },
-	{
-		"event": 3,
-		"type": "moment",
-        "data": {
-            "title": "Moment about king of the north"
+            "title": "Moment about king of the north",
+            "timestamp" : 85
         }
     },	
     {
-		"event": 4,
+		"event": 2,
+        "type": "character",
+        "data": {
+            "timestamp":90,
+            "characters":[{ "name": "Lord_Bolton", "family": "stark"},
+                            {"name": "Rob_Stark", "family": "stark"}]
+        }
+    },
+    
+    {
+		"event": 3,
         "type": "newScene",
         "data": {
-            "locationX": 870,
-            "locationY": 430,
-            "title": "The morning of victory",
-            "characters": ["Rob Start", "Lord Bolton"]
+            "locationX": 170,
+            "locationY": 150,
+            "characters": [{ "name": "Joffrey_Baratheon", "family": "baratheon"},
+                           {"name": "Sansa_Stark", "family": "stark"},
+                           {"name": "Lancel_Lannister", "family": "lannister"}]
+        }
+    },
+
+     {
+        "event": 4,
+        "type": "moment",
+        "data": {
+            "title": "Iron Throne",
+            "timestamp": 132
         }
     },
     {
 		"event": 5,
-		"type": "characterEntry",
+		"type": "character",
         "data": {
-            "character": "Nurse"
+            "timestamp": 170,
+            "characters":[{ "name": "Joffrey_Baratheon", "family": "baratheon"},
+                          {"name": "Sansa_Stark", "family": "stark"},
+                          {"name": "Meryn_Trent", "family": "trent"}]
+        }          
+    },
+    {
+        "event": 6,
+        "type": "character",
+        "timestamp": 204,
+        "data": {
+            "timestamp": 204,
+            "characters":[{"name": "Joffrey_Baratheon", "family": "baratheon"},
+                          {"name": "Sansa_Stark", "family": "stark"},
+                          {"name": "Tyrion_Lannister", "family": "lannister"}]
+        }
+        
+    },
+     {
+        "event": 7,
+        "type": "moment",
+        "data": {
+            "title": "White Cloak",
+            "timestamp": 222
         }
     },
     {
-		"event": 5,
+        "event": 8,
+        "type": "moment",
+        "data": {
+            "title": "Jaime killing Mad King",
+            "timestamp":238
+        }
+    },
+     {
+        "event": 8,
+        "type": "moment",
+        "data": {
+            "title": "Sansa's engagement to Joffrey",
+            "timestamp":288
+        }
+    },
+     {
+        "event": 9,
         "type": "newScene",
         "data": {
-            "locationX": 870,
-            "locationY": 430,
-            "title": ["Penalty of victory"],
-            "characters": ["Joffrey Baratheon", "Sansa Stark", "Lancel Lannister"]
+            "locationX": 670,
+            "locationY": 230,
+            "characters": [{"name": "Arya_Stark", "family": "stark"},
+                           {"name": "Hot_Pie", "family": "unknown"},
+                           {"name": "Gendry", "family": "baratheon"} ]
         }
     },
     {
-		"event": 6,
-		"type": "characterEntry",
+        "event": 10,
+        "type": "newScene",
         "data": {
-            "character": "Tyrion Lannister"
+            "locationX": 440,
+            "locationY": 430,
+            "characters": [{"name": "Catelyn_Stark", "family": "stark"},
+                           {"name": "Petyr_Baelish", "family": "unknown"},
+                           {"name": "Eddard_Stark", "family": "stark"}]
         }
-    }
+    },
+    {
+        "event": 11,
+        "type": "moment",
+        "data": {
+            "title": "Baelish betrayed Ned Stark",
+            "timestamp":448
+        }
+    },
+
+    {
+        "event": 12,
+        "type": "moment",
+        "data": {
+            "title": "Baelish betrayed Ned Stark",
+            "timestamp":465
+        }
+    },
+    
+    {
+        "event": 13,
+        "type": "moment",
+        "data": {
+            "title": "Silent Sisters",
+            "timestamp":584
+        }
+    },
+     {
+        "event": 14,
+        "type": "newScene",
+        "data": {
+            "locationX": 140,
+            "locationY": 530,
+            "characters": [{"name": "Catelyn_Stark", "family": "stark"},
+                           {"name": "Renly_Baratheon", "family": "baratheon"},
+                           {"name": "Stannis_Baratheon", "family": "baratheon"},
+                           {"name": "Red_Sorceress", "family": "unknown"}]
+        }
+    },
+     {
+        "event": 15,
+        "type": "moment",
+        "data": {
+            "title": "Baratheon crown/ symbol of stag",
+            "timestamp":683
+        }
+    },
+
+     {
+        "event": 16,
+        "type": "moment",
+        "data": {
+            "title": "Lord of light",
+            "timestamp":695
+        }
+    },
+     {
+        "event": 17,
+        "type": "moment",
+        "data": {
+            "title": "Salt and smoke",
+            "timestamp":725
+        }
+    },
+    {
+        "event": 18,
+        "type": "newScene",
+        "data": {
+            "locationX": 740,
+            "locationY": 330,
+            "characters": [{"name": "Daenerys_Targaryen", "family": "targaryen"},
+                           {"name": "Jorah_Mormon", "family": "unknown"},
+                           {"name": "Stannis_Baratheon", "family": "baratheon"},
+                           {"name": "Red_Sorceress", "family": "unknown"}]
+                        
+        }
+    },
+    {
+        "event": 19,
+        "type": "moment",
+        "data": {
+            "title": "Mother of dragons",
+            "timestamp":900
+        }
+    },
+    {
+        "event": 20,
+        "type": "moment",
+        "data": {
+            "title": "Trader of spices",
+            "timestamp":908
+        }
+    },
+
 ];
